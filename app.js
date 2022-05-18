3f93d3fc8b7a2e5695bcadae950627b504aaf3d1
Don <genericsalesman6@gmail.com>
2022-05-18 12:08:18 -0700

    Added Some styling, fixed listing functionality

--- app.js
+++ app.js
@@ -7,16 +7,22 @@ let listedBills = document.getElementById('bill-list')
 let billList= []
 
 submitButton.addEventListener('click',(e)=>{
-e.preventDefault()
-billList.push({billName: billName.value, billDate: billDate.value})
-console.log(billList)
-billList.forEach((bill)=>{
-    let li = document.createElement('li')
-    console.lo
-    li.innerHTML="<li>"+bill.billName+", "+ bill.billDate+"</li>"
-    listedBills.appendChild(li)
-    billName.value= ""
-    billDate.value= ""
-})
+    e.preventDefault()
+    billList.push({billName: billName.value, billDate: billDate.value})
+    console.log(billList)
 
+return billList.map((bill,i)=>{
+        console.log(billList[billList.length-1].billName == bill.billName)
+        console.log(billList[billList.length-1])
+    if (billList[billList.length-1].billName == bill.billName){
+        let li = document.createElement('li');
+
+        li.innerHTML="<li>"+bill.billName+", "+ bill.billDate+"</li>";
+        listedBills.appendChild(li);
+
+    }
+
+    billName.value= "";
+    billDate.value= "";
+})
 })
