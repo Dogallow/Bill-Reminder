ad07ad5e80a779dc66fc9bcc66216ac64331d0d5
Don <genericsalesman6@gmail.com>
2022-05-20 19:59:56 -0700

    Minor changes to the javascript

--- app.js
+++ app.js
@@ -16,13 +16,30 @@ return billList.map((bill,i)=>{
         console.log(billList[billList.length-1])
     if (billList[billList.length-1].billName == bill.billName){
         let li = document.createElement('li');
+        let button = document.createElement('button');
+        button.innerText="Delete";
 
-        li.innerHTML="<li>"+bill.billName+", "+ bill.billDate+"</li>";
+        li.innerHTML="<li>"+bill.billName+", "+ bill.billDate +"</li>";
+        li.appendChild(button)
         listedBills.appendChild(li);
+
+
+        button.addEventListener('click', ()=>{
+            //Need work modifying the array, so that the delete functionality works
+            console.log(billList, i)
+            listedBills.removeChild(li)
+
+            billList = billList.slice(i,i+1)
+            console.log(billList)
+        })
 
     }
 
     billName.value= "";
     billDate.value= "";
+    console.log(billList)
 })
 })
+ function removeListItem(list, index){
+    console.log("this func")
+ }
